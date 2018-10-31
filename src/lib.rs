75fe02c59a87d14dc50b1ce73b98c24b54c616a3
Min Kim <msk@dolbo.net>
2018-10-30 19:46:23 -0700

    Unroll the loop for dot

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,37 @@
+use std::cmp;
 use std::default::Default;
 use std::ops::{Add, AddAssign, Mul};
 
 pub fn dot<T>(x: &[T], y: &[T]) -> T
 where
-    T: Add + AddAssign + Mul<Output = T> + Copy + Default,
+    T: Add<Output = T> + AddAssign + Mul<Output = T> + Copy + Default,
 {
-    let mut sum: T = Default::default();
+    let len = cmp::min(x.len(), y.len());
+    let mut x = &x[..len];
+    let mut y = &y[..len];
+
+    let mut sum0: T = Default::default();
+    let mut sum1: T = Default::default();
+    let mut sum2: T = Default::default();
+    let mut sum3: T = Default::default();
+    let mut sum4: T = Default::default();
+    let mut sum5: T = Default::default();
+    let mut sum6: T = Default::default();
+    let mut sum7: T = Default::default();
+    while x.len() >= 8 {
+        sum0 += x[0] * y[0];
+        sum1 += x[1] * y[1];
+        sum2 += x[2] * y[2];
+        sum3 += x[3] * y[3];
+        sum4 += x[4] * y[4];
+        sum5 += x[5] * y[5];
+        sum6 += x[6] * y[6];
+        sum7 += x[7] * y[7];
+
+        x = &x[8..];
+        y = &y[8..];
+    }
+    let mut sum = sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7;
     for (&a, &b) in x.iter().zip(y) {
         sum += a * b;
     }
